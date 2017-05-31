321cd8a6287ad3e29714d5ab96f46068cdc7809e
MMattLeeE <MMattLeeE@gmail.com>
2017-05-31 15:51:34 -0400

    Add files via upload

---
+++ login/src/Login/ControllerRegister.java
@@ -0,0 +1,173 @@
+package Login;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Matt on 5/27/2017.
+ */
+public class ControllerRegister implements Initializable {
+
+    public Label firstNameLabel;
+    public Label lastNameLabel;
+    public Label dobLabel;
+    public Label genderLabel;
+    public Label newUsernameLabel;
+    public Label newPasswordLabel;
+    public Label confirmPasswordLabel;
+    public Label errorsLabel;
+    @FXML
+    private TextField dateInput;//required
+    @FXML
+    private TextField ssnInput;
+    @FXML
+    private TextField emailInput;
+    @FXML
+    private TextField phoneNumberInput;
+    @FXML
+    private TextField firstNameInput;//required
+    @FXML
+    private TextField lastNameInput;//required
+    @FXML
+    private TextField newUsernameInput;//required
+    @FXML
+    private TextField newPasswordInput;//required
+    @FXML
+    private TextField confirmNewPasswordInput;//required
+    @FXML
+    private Button registerBtn;
+    @FXML
+    private Button addPhotoBtn;
+    @FXML
+    private Button backLogin;
+    @FXML
+    private RadioButton radioMale;//required
+    @FXML
+    private RadioButton radioFemale;//required
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        registerBtn.setOnAction(event -> {
+            validateInputs();
+        });
+
+        backLogin.setOnAction(e -> {
+            try {
+                loadRegisterPage();
+
+            } catch (IOException ex) {
+                    ex.printStackTrace();
+            }
+        });
+    }
+
+    @FXML
+    private void validateInputs() {
+        if (!isMissingInput()) {
+            if (checkUsername() && checkEmail() && checkPassword() && checkDOB()) {
+
+            }
+        }
+    }
+
+    private boolean checkEmail() {
+        boolean output = false;
+        if (!emailInput.getText().isEmpty()) {
+
+        }
+        return output;
+    }
+
+    private boolean checkDOB() {
+        boolean output = false;
+
+        return output;
+    }
+
+    private boolean checkPassword() {
+        boolean output = false;
+
+        return output;
+    }
+
+    private boolean checkUsername() {
+        boolean output = false;
+
+        return output;
+    }
+
+    @FXML
+    private void loadRegisterPage() throws IOException{
+        Stage stage;
+        Parent root;
+
+        stage = (Stage) backLogin.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("loginPage.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private boolean isMissingInput() {
+        boolean output=false;
+
+        errorsLabel.setTextFill(Color.RED);
+
+        if (firstNameInput.getText().isEmpty()) {
+            firstNameLabel.setTextFill(Color.RED);
+            errorMessageBlankFields();
+            output = true;
+        }
+        if (lastNameInput.getText().isEmpty()) {
+            lastNameLabel.setTextFill(Color.RED);
+            errorMessageBlankFields();
+            output = true;
+        }
+        if (newUsernameInput.getText().isEmpty()) {
+            newUsernameLabel.setTextFill(Color.RED);
+            errorMessageBlankFields();
+            output = true;
+        }
+        if (newPasswordInput.getText().isEmpty()) {
+            newPasswordLabel.setTextFill(Color.RED);
+            errorMessageBlankFields();
+            output = true;
+        }
+        if (confirmNewPasswordInput.getText().isEmpty()) {
+            confirmPasswordLabel.setTextFill(Color.RED);
+            errorMessageBlankFields();
+            output = true;
+        }
+        if (!radioMale.isSelected() && !radioFemale.isSelected()) {
+            genderLabel.setTextFill(Color.RED);
+            errorMessageBlankFields();
+            output = true;
+        }
+        if (dateInput.getText().isEmpty()) {
+            dobLabel.setTextFill(Color.RED);
+            errorMessageBlankFields();
+            output = true;
+        }
+        return output;
+    }
+
+    @FXML
+    private void errorMessageBlankFields() {
+        errorsLabel.setText("Required information in red");
+        errorsLabel.setVisible(true);
+    }
+}
---
+++ login/src/Login/UserIO.java
@@ -0,0 +1,44 @@
+package Login;
+
+import java.io.*;
+import java.nio.file.*;
+/**
+ * Created by Matt on 5/30/2017.
+ */
+public class UserIO {
+
+    public static void writeUsers(Object data) throws IOException {
+        checkForDatFile();
+        ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream("users.dat"));
+        output.writeObject(data);
+    }
+
+    public static Object readUsers() throws IOException, ClassNotFoundException {
+        checkForDatFile();
+        ObjectInputStream input = new ObjectInputStream(new FileInputStream("users.dat"));
+        return input.readObject();
+    }
+
+    private static void checkForDatFile() {
+        Path path = Paths.get("users.dat");
+
+        if (Files.notExists(path)){
+            User firstUser = new User();
+            UserDB.getUsersArrayList().add(firstUser);
+
+            try {
+                File firstFile = new File("users.dat");
+                firstFile.createNewFile();
+                ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(firstFile, false));
+                output.writeObject(UserDB.getUsersArrayList());
+                output.flush();
+                output.close();
+
+            } catch (IOException e) {
+                System.err.println("cannot write to binary file");
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
